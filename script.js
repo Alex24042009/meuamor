17813c4a8ac765a3adece0493d8ff7f9b0b118b3
Alex24042009 <alexhenrique1928@gmail.com>
2025-08-19 09:06:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+const btnPlayMusic = document.getElementById('btnPlayMusic');
+const musica = document.getElementById('musica');
+
+btnPlayMusic.addEventListener('click', () => {
+  if (musica.paused) {
+    musica.play();
+    btnPlayMusic.textContent = 'Pausar música';
+  } else {
+    musica.pause();
+    btnPlayMusic.textContent = 'Ouvir música de fundo';
+  }
+});
